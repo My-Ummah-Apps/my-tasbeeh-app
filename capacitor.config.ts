62695e9090b192766482c7520c73dee1d9d867a4
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2023-04-06 15:51:49 +0100

    slight css tweaks, app uploaded to playstore open beta

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.tasbeeh.my',
-  appName: 'my-tasbeeh-app',
+  appName: 'My Tasbeeh App',
   webDir: 'build', 
   bundledWebRuntime: false,
   server: {
