85e2ef55c04aeadd7a9180cc996d03cc7c71eb30
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2023-05-22 07:31:13 +0100

    removed redundant code

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,23 +1,5 @@
 const SettingsPage = () => {
-  const changeThemeWhite = () => {
-    document.body.style = "background: #fff;";
-  };
-
-  const changeThemeBlack = () => {
-    document.body.style = "background: #000;";
-  };
-
-  return (
-    <div className="settings-page-wrap">
-      <div className="theme-selection-wrap">
-        <h1>Theme</h1>
-        <div className="theme-blocks-container">
-          <div className="white-theme-block" onClick={changeThemeWhite}></div>
-          <div className="black-theme-block" onClick={changeThemeBlack}></div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="settings-page-wrap"></div>;
 };
 
 export default SettingsPage;
