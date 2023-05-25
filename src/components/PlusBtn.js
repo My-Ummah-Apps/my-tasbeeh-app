c68a3051c6e69bc62e2b52d0fe9ae3af20f55a3c
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2023-05-25 08:25:37 +0100

    managed to get haptics working, when plus button is tapped device now vibrates, this now needs to be wired upso that the user can turn this functionality on and off via the settings page

--- src/components/PlusBtn.js
+++ src/components/PlusBtn.js
@@ -1,3 +1,29 @@
+import { Haptics, ImpactStyle } from "@capacitor/haptics";
+
+const hapticsImpactMedium = async () => {
+  await Haptics.impact({ style: ImpactStyle.Medium });
+};
+
+const hapticsImpactLight = async () => {
+  await Haptics.impact({ style: ImpactStyle.Light });
+};
+
+const hapticsVibrate = async () => {
+  await Haptics.vibrate();
+};
+
+const hapticsSelectionStart = async () => {
+  await Haptics.selectionStart();
+};
+
+const hapticsSelectionChanged = async () => {
+  await Haptics.selectionChanged();
+};
+
+const hapticsSelectionEnd = async () => {
+  await Haptics.selectionEnd();
+};
+
 const PlusBtn = ({
   setActiveCounterNumber,
   activeCounterNumber,
@@ -7,6 +33,9 @@ const PlusBtn = ({
 }) => {
   return (
     <div
+      onClick={() => {
+        hapticsImpactMedium();
+      }}
       className={`plus-svg-wrap ${showAnimation ? "fade-up-animation" : null}`}
     >
       <svg
