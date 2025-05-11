c294545d6108ebf384bc75e7fc5b91729c41789f
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2025-05-11 15:59:38 +0100

    chore(android): update Android config

--- dist/assets/web-BNqGfr8s.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-DOTyvE5s.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
--- dist/assets/web-Duq76ECM.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./index-DOTyvE5s.js";class i extends e{async requestReview(){throw this.unimplemented("Not implemented on web.")}}export{i as InAppReviewWeb};
