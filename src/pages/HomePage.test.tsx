67bbbce6637a56cdba415479a1982412309ebc73
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2025-10-17 16:22:13 +0100

    test: add tests for homepage progress bar

--- src/pages/HomePage.test.tsx
+++ src/pages/HomePage.test.tsx
@@ -1,7 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import { vi } from "vitest";
+
 import {
   mockColor,
   mockedCounterObj,
@@ -9,9 +10,9 @@ import {
   mockedScrollSpeed,
   mockedSetLanguageDirection,
   mockedSetScrollSpeed,
+  mockedUserPreferencesState,
 } from "../components/__mocks__/mockData";
-
-const setActiveCounter = vi.fn();
+import userEvent from "@testing-library/user-event";
 
 const mockedLanguageDirection = "ltr";
 
@@ -21,7 +22,11 @@ const mockedDbConnection = {
     close: vi.fn().mockResolvedValue(undefined),
   } as any,
 };
+
 const mockedToggleDBConnection = vi.fn().mockResolvedValue(undefined);
+const mockedUpdateActiveCounter = vi.fn().mockResolvedValue(undefined);
+const mockedUpdateCountersState = vi.fn().mockResolvedValue(undefined);
+const mockedCountersState = [mockedCounterObj];
 
 describe("integration tests", () => {
   beforeEach(() => {
@@ -30,24 +35,41 @@ describe("integration tests", () => {
         <HomePage
           dbConnection={mockedDbConnection}
           toggleDBConnection={mockedToggleDBConnection}
-          updateActiveCounter={updateActiveCounter}
+          updateActiveCounter={mockedUpdateActiveCounter}
           activeColor={mockColor}
           activeCounter={mockedCounterObj}
           resetSingleCounter={mockedResetSingleCounter}
-          updateCountersState={updateCountersState}
-          countersState={countersState}
+          updateCountersState={mockedUpdateCountersState}
+          countersState={mockedCountersState}
           setLanguageDirection={mockedSetLanguageDirection}
           languageDirection={mockedLanguageDirection}
           setScrollSpeed={mockedSetScrollSpeed}
           scrollSpeed={mockedScrollSpeed}
+          userPreferencesState={mockedUserPreferencesState}
           // animationDelay={1700}
         />
       </MemoryRouter>
     );
   });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("increases progress when counter button is tapped", () => {
+    const progressText = screen.getByText("50%");
+    expect(progressText).toBeInTheDocument();
+    expect(progressText).toHaveTextContent("50%");
+    const counterText = screen.getAllByText(mockedCounterObj.name)[0];
+    expect(counterText).toBeInTheDocument();
+    // const counterBtn = screen.getByText("of 10");
+    const counterBtn = screen.getByText("of 10");
+    console.log("counterBtn: ", counterBtn);
+
+    userEvent.click(counterBtn);
+    expect(progressText).toHaveTextContent("50%");
+  });
 });
 
 // Upon button being tapped, does the percentage increase
 // When reset button is tapped, does the count return to 0
-
-it("increases progress when counter button is tapped", () => {});
